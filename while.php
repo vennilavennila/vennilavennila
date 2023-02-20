e017bf746d409159cce12235dcc977721e4d17ed
vennilavennila <118593328+vennilavennila@users.noreply.github.com>
2023-02-20 20:02:07 +0530

    php

--- while.php
+++ while.php
@@ -10,6 +10,13 @@
         echo "The Even Number is : $num<br>";
         $num+=2;
     }
+
+    $area=100;
+    while($area>=0){
+        echo"Decrease the numbers : $area<br>";
+        $area--;
+        $area-=9;
+    }
     ?>
 </body>
 </html>
